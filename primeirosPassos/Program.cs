06659d0cd8432359d6f9cade78942dc306c9aee4
daniela marques <dd978452@gmail.com>
2023-01-10 13:44:26 -0300

    Adicionar arquivos de projeto.

---
+++ primeirosPassos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace primeirosPassos
+{
+    class Program
+    {
+        static void Main(string[] args) //porta de entrada para o programa
+        {
+            string nome = "";
+            Console.WriteLine("Escreva seu nome: ");
+            nome  = Console.ReadLine();
+            Console.WriteLine("seu nome é: ");
+            Console.WriteLine(nome);
+        }
+    }
+}
